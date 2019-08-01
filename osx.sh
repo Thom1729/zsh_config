dd517df3c487b14689008c44af209f3efc07034f
Thomas Smith <thom@thomthesmith.com>
2019-08-01 14:49:14 -0400

    Add Mac sound effects.

--- osx.sh
+++ osx.sh
@@ -4,3 +4,17 @@ export SUBLIME_COMMAND="/Applications/Sublime Text.app/Contents/SharedSupport/bi
 # GNU Coreutils
 
 export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
+
+function play-sound {
+    afplay "/System/Library/Sounds/$1.aiff" &!
+}
+
+function ding {
+    local last_status=$?
+    if [ $last_status -eq 0 ]; then
+        play-sound Hero
+    else
+        play-sound Sosumi
+        return $last_status
+    fi
+}
